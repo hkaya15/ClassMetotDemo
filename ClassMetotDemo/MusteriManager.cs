b7304fb44235510ab1fddcb61cabbf922ae2aaa2
Hüseyin Kaya <kayahuseyin15@gmail.com>
2021-01-18 00:50:49 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public static List<Musteri> musteriListesi = new List<Musteri>();
+        
+        public void CustomerAdd(Musteri musteri)
+        {
+            musteriListesi.Add(musteri);
+            Console.WriteLine(musteri.Ad+musteri.Soyad+"Eklendi");
+            
+ 
+
+        }
+
+        public void CustomerList()
+        {
+            Console.WriteLine("Müşteri Listesi");
+            foreach (Musteri musteriler in musteriListesi)
+            {
+                Console.WriteLine(musteriler.Id);
+                Console.WriteLine(musteriler.Ad);
+                Console.WriteLine(musteriler.Soyad);
+                Console.WriteLine(musteriler.Balance);
+
+                Console.WriteLine("********");
+            }
+        }
+
+        public void CustomerDelete(int Id)
+        {
+            foreach (var item in musteriListesi)
+            {
+                if (item.Id == Id)
+                {
+                    musteriListesi.Remove(item);
+                    Console.WriteLine("Müşteri Silindi");
+                    break;
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Musteri musteri = new Musteri();
+                MusteriManager musteriManager = new MusteriManager();
+                Console.WriteLine("1-Müşteri Ekle");
+                Console.WriteLine("2-Müşteri Listesi");
+                Console.WriteLine("3-Müşteri Sil");
+                Console.WriteLine("4-Çıkış");
+                var secim = Console.ReadLine();
+                var sonuc = Convert.ToInt32(secim);
+
+
+                if (sonuc == 1)
+                {
+             
+                        Console.WriteLine("Müşteri ID : ");
+                        var id1 = Console.ReadLine();
+                        var id = Convert.ToInt32(id1);
+                        Console.WriteLine("Müşteri Adı : ");
+                        var ad = Console.ReadLine();
+                        Console.WriteLine("Müşteri Soyad : ");
+                        var soyad = Console.ReadLine();
+                        Console.WriteLine("Müşteri Bakiye : ");
+                        var bakiye1 = Console.ReadLine();
+                        var bakiye = Convert.ToDouble(bakiye1);
+
+                        musteri.Id = id;
+                        musteri.Ad = ad;
+                        musteri.Soyad = soyad;
+                        musteri.Balance = bakiye;
+
+                        musteriManager.CustomerAdd(musteri);
+
+                   
+
+
+
+
+
+
+                }
+                else if (sonuc == 2)
+                {
+                    musteriManager.CustomerList();
+                }
+                else if (sonuc == 3)
+                {
+                    Console.WriteLine("Silmek istediğiniz Müşteri ID'si : ");
+                    var id1 = Console.ReadLine();
+                    var id = Convert.ToInt32(id1);
+                    musteriManager.CustomerDelete(id);
+                }
+                else if (sonuc == 4)
+                {
+                    Console.WriteLine("Çıkış Yapılıyor");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz İşlem");
+                }
+            }
+           
+            
+            
+                
+               
+            
+        }
+        
+    }
+}
